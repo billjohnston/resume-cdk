c12a43226259e4b0736001924f66ec231ca136cb
Bill Johnston <billjohnston@pm.me>
2020-10-15 15:10:46 +0000

    Linting

--- src/bin/stacks.ts
+++ src/bin/stacks.ts
@@ -1,18 +1,15 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import WebHost from 'lib/WebHost';
+import 'source-map-support/register'
+import * as cdk from '@aws-cdk/core'
+import WebHost from 'lib/WebHost'
 
-const app = new cdk.App();
-new WebHost(
-    app,
-    'BillResumeWebHost',
-    {
-        env: {
-            region: 'us-east-1',
-            account: '829151516027',
-        },
-        apexDomain: 'billjohnston.co',
-        certificateArn: 'arn:aws:acm:us-east-1:829151516027:certificate/39154a87-4c3d-485e-82d6-2f6ab86f1e64',
-    }
-);
+const app = new cdk.App()
+new WebHost(app, 'BillResumeWebHost', {
+    env: {
+        region: 'us-east-1',
+        account: '829151516027',
+    },
+    apexDomain: 'billjohnston.co',
+    certificateArn:
+        'arn:aws:acm:us-east-1:829151516027:certificate/39154a87-4c3d-485e-82d6-2f6ab86f1e64',
+})
